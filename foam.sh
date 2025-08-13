5deb934c503109732c4ecd8b626b92029bdab326
Avery <75178339+as-hpc@users.noreply.github.com>
2025-08-13 13:49:19 -0500

    Create foam.sh

---
+++ foam.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+## Set programming environment
+module unload craype-hugepages2M
+module load PrgEnv-cray
+module load openmpi
+
+## Clone the OpenFOAM 8 repo
+git clone https://github.com/OpenFOAM/OpenFOAM-8.git
+git clone https://github.com/OpenFOAM/ThirdParty-8.git
+
+## Go to where OpenFOAM should be installed
+cd OpenFOAM-8
+
+## Source the bashrc file
+source /etc/bashrc
+
+## Compile with 16 processors or more
+./Allwmake -j16
